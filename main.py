33ed81f1d4b894e25f2faef57031000975f5e2ff
neel-jotaniya <jotaniyaneel07@gmail.com>
2022-04-03 18:01:48 +0530

    Change database to postgresql

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)#mysql://username:password@server/db
+app = Flask(__name__)#mysql://fkobrltakxhyef:7df23ed83fcd49d0b3d61296506a64684cdcb39a1e5f90fa91f3a4f51c0ff87a@server/db
 
+email = ['jotaniyaneel07@gmail.com','jotaniyakrish07@gmail.com']
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fkobrltakxhyef:7df23ed83fcd49d0b3d61296506a64684cdcb39a1e5f90fa91f3a4f51c0ff87a@ec2-52-73-155-171.compute-1.amazonaws.com:5432/dcodrl73rb0c4k'
 
 db = SQLAlchemy(app)
 class Content(db.Model):
@@ -15,6 +16,13 @@ class Content(db.Model):
     Wallpaper_description = db.Column(db.String(1000),nullable=False)
     SNO = db.Column(db.Integer, primary_key=True,nullable=False)
     Wallpaper_compatibility = db.Column(db.String(1000),nullable=False)
+    
+class Feedback(db.Model):
+    name = db.Column(db.String(1000),nullable=False)
+    rating = db.Column(db.String(1000),nullable=False)
+    suggestion = db.Column(db.String(1000),nullable=False)
+    SNO = db.Column(db.Integer, primary_key=True,nullable=False)
+    
 
     
 db.init_app(app)
@@ -23,6 +31,18 @@ db.create_all()
 @app.route('/',methods = ['GET','POST'])
 def home():
     all_image = Content.query.all()
+    if request.method == 'POST':
+        name = request.form.get('name')
+        rating = request.form.get('rating')
+        suggestion = request.form.get('message')
+        feedback_form = Feedback(name = name,rating = rating,suggestion = suggestion)
+        db.session.add(feedback_form)
+        db.session.commit()
+
+        return render_template('index.html', all_image = all_image)
+
+        
+             
     return render_template('index.html', all_image = all_image)
 
 @app.route('/admin',methods = ['GET','POST'])
@@ -47,6 +67,22 @@ def private_route():
         return redirect('/')
     else:
         return render_template('admin.html')
+    
+@app.route('/login',methods = ['GET','POST'])
+def login():
+    if request.method == 'POST':
+        id = request.form.get('uniqueid')
+        password = request.form.get('inputpassword')
+        if id in email and password == '101010':
+            return render_template('admin.html')
+        else :
+            return render_template('login.html')
+    else :
+        return render_template('login.html')
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+        
 
 
 
